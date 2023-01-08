912f6abc146f577341cbb344d885e352ea9f0748
shivang0-0 <shivang.sharma11@pm.me>
2023-01-08 16:57:20 +0530

    server connection achieved, rendering index.ejs

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+const app = express()
+
+//connect to the mongodb database...
+mongoose.connect("mongodb://localhost/blog_it", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log("Connection to mongodb was successfull!!");
+})
+
+//middlewares
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
+app.set("view engine","ejs")
+
+//routes
+app.use(require("./routes/index"))
+
+
+//server configuration
+app.listen(3000, () => console.log("Listening at port 3000"))
